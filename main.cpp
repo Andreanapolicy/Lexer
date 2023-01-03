4f1732ba3adf88171808e02d110e30613e87bb20
Andreanapolicy <daa.and@mail.ru>
2023-01-03 13:24:45 +0300

    Lab7. Проинициализировал репозиторий с лексером

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+
+namespace
+{
+struct Args
+{
+	std::string inputFilename;
+};
+
+Args ParseArgs(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		throw std::invalid_argument("invalid parameters");
+	}
+
+	return {
+		.inputFilename = argv[1]
+	};
+}
+} // namespace
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		auto const args = ParseArgs(argc, argv);
+		std::ifstream input(args.inputFilename);
+
+		if (!input.is_open())
+		{
+			throw std::runtime_error("Cannot open input file");
+		}
+		std::string data((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+	}
+	catch (std::exception const& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
