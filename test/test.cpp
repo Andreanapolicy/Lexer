a063c6a7f04232d925cde05c7d2c5aae8a9f57b0
Andreanapolicy <daa.and@mail.ru>
2023-01-14 13:52:16 +0300

    Lab7. Добавил первые тесты на комментарий

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,50 @@
 #define CATCH_CONFIG_MAIN
+
 #include "../catch2/catch.hpp"
+#include "../src/Lexer/Lexer.h"
+#include "../src/utils/LexerDialog/LexerDialog.h"
+
+TEST_CASE("Tests for single usage lexems")
+{
+	SECTION("Testing comments")
+	{
+		GIVEN("Lexer dialog, input and output")
+		{
+			std::stringstream input;
+			std::stringstream output;
+
+			auto lexerDialog = std::make_unique<LexerDialog>(output);
+
+			WHEN("In lexer goes single comment with right usage")
+			{
+				input << "// this is a comment";
+				auto lexer = std::make_unique<lexer::Lexer>(input);
+				lexer->Process();
+
+				THEN("List of token will be empty")
+				{
+					std::stringstream intendedOutput;
+
+					lexerDialog->OutputTokens(lexer->GetAllTokens());
+					REQUIRE(output.str() == intendedOutput.str());
+				}
+			}
+
+			WHEN("In lexer goes double comments with right usage")
+			{
+				input << "// this is a comment" << std::endl
+					  << "// and this is too comment";
+				auto lexer = std::make_unique<lexer::Lexer>(input);
+				lexer->Process();
+
+				THEN("List of token will be empty")
+				{
+					std::stringstream intendedOutput;
+
+					lexerDialog->OutputTokens(lexer->GetAllTokens());
+					REQUIRE(output.str() == intendedOutput.str());
+				}
+			}
+		}
+	}
+}
